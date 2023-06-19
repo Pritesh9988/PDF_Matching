a6876867c631eeb11bb0bd552ec8d8f9ddbac4ea
Pritesh <priteshlonkar007@gmail.com>
2023-06-19 09:33:32 +0530

    added

--- text highlight.py
+++ text highlight.py
@@ -17,14 +17,18 @@ def highlight_differences(file1, file2):
 
         # Perform text comparison
         differ = difflib.Differ()
-        diff = differ.compare(text1.split(), text2.split())
+        diff = differ.compare(text1, text2)
 
-        # Highlight the differences on the second page
+        # Highlight the first occurrence of each difference on the second page
+        pos2 = 0
         for line in diff:
             if line.startswith('+'):
                 word2 = line[2:]
-                for span in page2.search_for(word2):
-                    page2.add_highlight_annot(span)
+                start = text2.find(word2, pos2)
+                if start != -1:
+                    end = start + len(word2)
+                    page2.add_highlight_annot(fitz.Rect(start, 0, end, 0))
+                    pos2 = end
 
     # Save the modified PDF
     output_path = "highlighted_differences.pdf"
@@ -34,6 +38,6 @@ def highlight_differences(file1, file2):
     print(f"Differences highlighted in {output_path}")
 
 # Compare two PDF files and highlight differences in the second PDF
-file1 = "file1.pdf"
-file2 = "file2.pdf"
+file1 = "path/to/file1.pdf"
+file2 = "path/to/file2.pdf"
 highlight_differences(file1, file2)
