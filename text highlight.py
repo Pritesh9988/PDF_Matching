a31abdb1255bfedfb5078e8013da20d1ce7ca4cc
Pritesh <priteshlonkar007@gmail.com>
2023-06-22 18:21:24 +0530

    updated

--- text highlight.py
+++ text highlight.py
@@ -1,45 +1,66 @@
-import fitz
-import difflib
+import PyPDF2
+import aspose.words as aw
+from datetime import date
 
-def highlight_differences(file1, file2):
-    # Open the PDF files
-    pdf1 = fitz.open(file1)
-    pdf2 = fitz.open(file2)
+def split_pdf_into_pages(input_path,input_path2):
+    # Open the PDF file
+    pdf = PyPDF2.PdfReader(input_path)
+    pdf2 = PyPDF2.PdfReader(input_path2)
 
-    # Iterate through the pages of the second PDF
-    for page_num in range(len(pdf2)):
-        page2 = pdf2[page_num]
+    # Iterate through each page
+    for page_num in range(len(pdf.pages)):
+        # Create a new PDF writer
+        pdf_writer = PyPDF2.PdfWriter()
+        pdf_writer2 = PyPDF2.PdfWriter()
+        # Add the current page to the writer
+        pdf_writer.add_page(pdf.pages[page_num])
+        pdf_writer2.add_page(pdf2.pages[page_num])
+        # Create the output file path
+        output_path = f"page_{page_num+1}.pdf"
+        output_path2 = f"page2_{page_num+1}.pdf"
+        # Write the output file
+        with open(output_path, "wb") as output_file:
+            pdf_writer.write(output_file)
+            
+        with open(output_path2, "wb") as output_file2:
+            pdf_writer.write(output_file2)
 
-        # Get the text from both pages
-        text1 = pdf1[page_num].get_text("text")
-        text2 = page2.get_text("text")
+        print(f"Page {page_num+1} saved as {output_path}")
+        print(f"Page {page_num+1} saved as {output_path2}")
+        
+        
 
-        # Compare the text using difflib
-        differ = difflib.Differ()
-        diff = differ.compare(text1, text2)
 
-        # Track the positions of differences
-        diff_positions = []
-        pos2 = 0
-        for line in diff:
-            if line.startswith('-') or line.startswith('+'):
-                diff_positions.append(pos2)
-            if not line.startswith('-'):
-                pos2 += len(line[2:])
+# Load PDF files
+        PDF1 = aw.Document(output_path)
+        PDF2 = aw.Document(output_path2)
 
-        # Highlight the positions of differences on the second page
-        for pos in diff_positions:
-            highlight_rect = [pos, 0, pos+1, 1]  # Modify the height as needed
-            page2.add_highlight_annot(highlight_rect)
+        # Convert PDF files to Word format
+        PDF1.save("first.docx", aw.SaveFormat.DOCX)
+        PDF2.save("second.docx", aw.SaveFormat.DOCX)
 
-    # Save the modified PDF
-    output_path = "highlighted_differences.pdf"
-    pdf2.save(output_path)
-    pdf2.close()
+        # Load converted Word documents 
+        DOC1 = aw.Document("first.docx")
+        DOC2 = aw.Document("second.docx")
 
-    print(f"Differences highlighted in {output_path}")
+# Set comparison options
+        options = aw.comparing.CompareOptions()            
 
-# Compare two PDF files and highlight the positions of differences in the second PDF
-file1 = "path/to/file1.pdf"
-file2 = "path/to/file2.pdf"
-highlight_differences(file1, file2)
+
+        # DOC1 will contain changes as revisions after comparison
+        DOC1.compare(DOC2, "user", date.today(), options)
+
+        if (DOC1.revisions.count >= 0):
+            # Save resultant file as PDF
+            DOC1.save(f"compare_op_{page_num+1}.pdf", aw.SaveFormat.PDF)
+        else:
+            print("Documents are equal")
+        
+        
+        
+        
+# Provide the input PDF file path
+input_file = "file1.pdf"
+input_file2 = "file2.pdf"
+# Split the PDF into separate pages
+split_pdf_into_pages(input_file,input_file2)
